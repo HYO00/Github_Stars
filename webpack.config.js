82f7bdad2d28b987bba5590a67cdf4d4986cf9ba
hyoyoung kim <hyoyoungkim@hyoyoungui-MacBook-Pro.local>
2022-01-09 01:33:28 +0900

    webpack setting

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); //설치한 웹팩 html 플러그인을 불러온다.
+
+module.exports = {
+  //entry 시작파일 output 결과물
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"), //현재경로 하위 dist 폴더를 의미
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ], //option으로 template  기존의 만들어 놨던 파일을 이용해서 html 파일을 만들어준다.
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+  },
+};
